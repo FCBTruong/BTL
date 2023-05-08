3ae75ee2c02b3e7284355ed7af7aa25c4c5a4e52
Truong <55469561+FCBTruong@users.noreply.github.com>
2023-05-08 21:51:17 +0700

    update use cv2 instead of onnxruntime

--- btl_test/landing_detector.py
+++ btl_test/landing_detector.py
@@ -1,8 +1,5 @@
-import torch
-import os
 import cv2
 import numpy as np
-import onnxruntime as rt
 
 class LandingDetector:
 
@@ -11,26 +8,27 @@ class LandingDetector:
         You should hard fix all the requirement parameters
         """
         self.name = 'LandingDetector'
-    def detect(self, image):
+
         # Load the YOLO model
         model_file = 'model/best.onnx'
-        session = rt.InferenceSession(model_file)
+        self.yolo_net = cv2.dnn.readNetFromONNX(model_file)
 
-        # Get the names of the model's input nodes
-        input_names = [input.name for input in session.get_inputs()]
-
+    def detect(self, image):
         # Preprocess the image
         input_size = (544, 544)
         resized_image = cv2.resize(image, input_size)
         input_data = np.transpose(resized_image, [2, 0, 1]) / 255.0
         input_data = np.expand_dims(input_data, axis=0).astype('float32')
 
-        # Run inference
-        output = session.run([], {input_names[0]: input_data})
+        # Set the input and output nodes for the YOLO network
+        self.yolo_net.setInput(input_data)
+        output_names = self.yolo_net.getUnconnectedOutLayersNames()
 
+        # Run inference
+        output = self.yolo_net.forward(output_names)
 
         output_array = output[0]
-        max_confidence_idx = np.argmax(output_array[:, :, 4])
+        max_confidence_idx = np.argmax(output_array[0, :, 4])
         max_confidence_bbox = output_array[0, max_confidence_idx, :]
         print(max_confidence_bbox)
         x_center, y_center, width, height, _, _ = max_confidence_bbox
@@ -40,6 +38,7 @@ class LandingDetector:
         y_min = y_center - height / 2
         x_max = x_center + width / 2
         y_max = y_center + height / 2
+
         return x_min, y_min, x_max, y_max
 
         # Do something with the output
